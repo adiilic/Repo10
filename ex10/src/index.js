c90402b09e225ca091ab5c2772a4195c241bb8e5
adiilic <adiilic12@gmail.com>
2021-09-27 00:35:41 +0200

    Ponavljanje

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -2,8 +2,8 @@ function myFunction() {
     var myMusic = {
         001: {
             artist: "Billy Joel",
-            tittle: "Piano Man",
-            relese_year: 1973,
+            title: "Piano Man",
+            release_year: 1973,
             formats: {
                 1: "CD",
                 2: "8T",
@@ -12,20 +12,26 @@ function myFunction() {
             gold: true
         },
         002: {
-            artist: "Jilly Boel",
-            tittle: "Miano Pan",
-            relese_year: 1937,
+            artist: "Celine Dion",
+            title: "Courage",
+            release_year: 2019,
             id: 100,
-            format: {
-                1: "DVD",
-                2: "CD",
-                3: "HDD"
-            }
-        }
+            formats: {
+                1: "CD",
+                2: "DVD",
+                3: "LP"
+            },
+            gold: true
 
+            // Add a record here
+
+        }
     };
+
     return myMusic;
+
 }
+
 myFunction()[2];
 
 module.exports = myFunction;
